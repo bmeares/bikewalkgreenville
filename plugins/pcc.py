27ca1b618c23c2ffbbbc3fe2313dacfda3266951
bmeares <bennett.meares@gmail.com>
2025-04-02 00:19:33 -0400

    Update pcc plugin and project

--- plugins/pcc.py
+++ plugins/pcc.py
@@ -7,9 +7,17 @@ Parse the data from PCC.
 
 import meerschaum as mrsm
 
+__version__ = '0.0.1'
+
 required: list[str] = ['pandas', 'pyarrow', 'geopandas', 'pyogrio']
 
 bwg = mrsm.Plugin('bwg')
+
+KNOWN_DTYPES: dict[str, str] = {
+    'COUNTY_ID': 'Int32',
+    'ROUTE_NUMB': 'Int32',
+    'fc_type': 'Int8',
+}
 
 
 def fetch(pipe: mrsm.Pipe):
@@ -23,6 +31,8 @@ def fetch(pipe: mrsm.Pipe):
     )
     data_path = bwg.module.get_data_path()
     pcc_path = data_path / 'Road Stress GIS Data from PCC'
-    unzip_path = pcc_path / 'PCC_Road_Stress_Public'
-    file_path = unzip_path / 'all_roads_with_fc_web.shp'
-    return gpd.read_file(file_path)
+    zip_path = pcc_path / 'PCC_Road_Stress_Public.zip'
+    gdf = gpd.read_file(zip_path)
+    for col, typ in KNOWN_DTYPES.items():
+        gdf[col] = gdf[col].astype(typ)
+    return gdf
