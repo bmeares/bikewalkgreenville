6c7bac7ce6ec7f1ea8746c24ae138b528d3e818c
bmeares <bennett.meares@gmail.com>
2025-10-15 16:41:39 -0400

    Improve search and add suggestions

--- plugins/who-owns-the-roads.py
+++ plugins/who-owns-the-roads.py
@@ -10,7 +10,7 @@ import meerschaum as mrsm
 from meerschaum.config import get_plugin_config, write_plugin_config
 from meerschaum.plugins import web_page, dash_plugin
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 required: list[str] = ['dash-leaflet']
 
@@ -23,13 +23,22 @@ TYPES_COLORS: dict[str, str] = {
     'Subdivision': '#577DAB',
 }
 
+SUFFIX_ALIASES: dict[str, str] = {
+    'street': 'st',
+    'road': 'rd',
+    'avenue': 'ave',
+    'court': 'ct',
+}
+
 
 @dash_plugin
 def init_dash(dash_app):
     """Initialize the Plotly Dash application."""
 
     import json
-    
+    import re
+    from urllib.parse import parse_qs
+
     import dash.html as html
     import dash.dcc as dcc
     import plotly.express as px
@@ -43,6 +52,7 @@ def init_dash(dash_app):
 
     from meerschaum.utils.dtypes import serialize_geometry
     from meerschaum.utils.dataframe import query_df
+    from meerschaum.api.dash.components import build_cards_grid
 
     roads_pipe = mrsm.Pipe('sql:bwg', 'roads', 'Roads', instance='sql:bwg')
     link_prefixes = {
@@ -67,22 +77,36 @@ def init_dash(dash_app):
     @dash_app.callback(
         Output('who-owns-the-roads-output-div', 'children'),
         Input('who-owns-the-roads-location', 'pathname'),
+        State('who-owns-the-roads-location', 'search'),
     )
-    def render_page_on_url_change(pathname: str):
+    def render_page_on_url_change(pathname: str, search: str):
         """Reload page contents when the URL path changes."""
-        form = html.Div([
-            dbc.Input(
-                placeholder="Road Name",
-                id="wotr-road-name-input",
-                type='text',
-            ),
-        ])
+        url_params = {
+            param: val[0]
+            for param, val in parse_qs(search.lstrip('?')).items()
+            if val
+        }
+        initial_search_value = url_params.get('search', '')
         submit_button = dbc.Button(
             "Search",
             id="wotr-submit-button",
             n_clicks=0,
         )
-        initial_map_layout = build_initial_map_layout()
+        form = html.Div([
+            dbc.InputGroup([
+                dbc.Input(
+                    value=initial_search_value,
+                    placeholder="Road Name",
+                    id="wotr-road-name-input",
+                    type='text',
+                    debounce=30,
+                ),
+                submit_button,
+            ]),
+            html.Div(id='wotr-search-suggestions'),
+        ])
+        iframe_scroll = url_params.get('embed', None) in ('true', 'True', '1')
+        initial_map_layout = build_initial_map_layout(iframe_scroll=iframe_scroll)
         return [
             dcc.Store(id='wotr-click-store'),
             html.Br(),
@@ -90,26 +114,31 @@ def init_dash(dash_app):
                 dbc.Col(form),
             ]),
             html.Br(),
-            dbc.Row([
-                dbc.Col([submit_button]),
-            ]),
-            html.Br(),
             initial_map_layout,
             html.Div(id="wotr-results-table"),
         ]
 
     @dash_app.callback(
-        Output('wotr-results-table', 'children'),
         Output('wotr-lines-group', 'children'),
-        Input('wotr-submit-button', 'n_clicks'),
-        Input('wotr-road-name-input', 'n_submit'),
-        State('wotr-road-name-input', 'value'),
+        Input({'type': 'wotr-card-name-button', 'ix': ALL}, 'n_clicks'),
+        Input({'type': 'wotr-search-suggest-button', 'ix': ALL}, 'n_clicks'),
+        prevent_initial_call=False,
     )
-    def submit_click(n_clicks, n_submit, road_name):
-        if not (n_clicks or n_submit) or not road_name:
+    def card_name_click(card_names_n_clicks, search_suggest_n_clicks):
+        if not any(card_names_n_clicks + search_suggest_n_clicks):
             raise PreventUpdate
 
-        road_name_clean = road_name.strip("'").strip(";").strip('-').lower()
+        try:
+            click_ix_val = callback_context.triggered_id['ix']
+            road_dict = json.loads(click_ix_val)
+        except Exception:
+            raise PreventUpdate
+
+        mrsm.pprint(road_dict)
+        road_name = road_dict['Name']
+        owner = road_dict.get('Owner', None)
+        road_type = road_dict.get('Type', None)
+        road_name_clean = get_road_name_clean(road_name)
         query = (
             "SELECT\n"
             "  \"Name\",\n"
@@ -121,8 +150,15 @@ def init_dash(dash_app):
             "  ST_Union(\"geometry\") AS \"geometry\"\n"
             "FROM \"Roads\".roads\n"
             "WHERE\n"
-            "  LOWER(\"Name\") LIKE '%%" + road_name_clean + "%%'\n"
-            "GROUP BY\n"
+            "  LOWER(REGEXP_REPLACE(\"Name\", '[^a-zA-Z0-9 ]', '', 'g')) = '" + road_name_clean + "'\n" + (
+                ("  AND \"Owner\" = '" + owner + "'\n")
+                if owner
+                else ""
+            ) + (
+                ("  AND \"Type\" = '" + road_type + "'\n")
+                if road_type
+                else ""
+            ) + "GROUP BY\n"
             "  \"Name\",\n"
             "  \"Type\",\n"
             "  \"Owner\",\n"
@@ -133,9 +169,28 @@ def init_dash(dash_app):
             "ORDER BY \"Name\" ASC\n"
             "LIMIT 100"
         )
+        print(query)
         df = gpd.read_postgis(query, roads_pipe.instance_connector.engine, geom_col='geometry')
+        road_types_geojson = get_road_types_geojson(df)
+        return list(road_types_geojson.values())
+
+    def get_road_name_clean(input_value: str) -> str:
+        """
+        Return the cleaned, normalized road name from the input string.
+        """
+        road_name_clean = re.sub(r'[^a-zA-Z0-9]', ' ', input_value)
+        road_name_clean = re.sub(r'\s+', ' ', road_name_clean).lower()
+        name_parts = road_name_clean.split(' ')
+        return ' '.join([
+            SUFFIX_ALIASES.get(part, part)
+            for part in name_parts
+        ])
+
+    def get_road_types_geojson(df, layer_type: str = 'wotr-lines'):
+        """
+        Get the road types to GeoJSON dictionary from a query result dataframe.
+        """
         road_types = df['Type'].unique()
-        non_geo_cols = [col for col in df.columns if col != 'geometry']
         road_types_geojson_data = {}
         for road_type in road_types:
             try:
@@ -145,40 +200,14 @@ def init_dash(dash_app):
             except ValueError:
                 pass
 
-        table = dbc.Table(
-            [html.Thead([html.Tr([html.Th(col) for col in non_geo_cols])])] + [html.Tbody([
-                html.Tr([
-                    html.Td(
-                        val
-                        if (link_prefix := link_prefixes.get(col)) is None
-                        else (
-                            html.A(val, href=(link_prefix + val), style={'textDecoration': 'none'})
-                            if col != 'Online Form'
-                            else html.A(
-                                href=val,
-                                target="_blank",
-                                children=[dbc.Button("Report an Issue", class_name="w-100")],
-                                style={'textDecoration': 'none'},
-                            )
-                        )
-                    ) if ((val := doc.get(col)) and val != 'N/A') else html.Td()
-                    for col in non_geo_cols
-                ])
-                for doc in df[non_geo_cols].to_dict(orient='records')
-            ])],
-            striped=True,
-            bordered=True,
-            hover=True,
-        )
-
-        road_types_geojson = {
+        return {
             road_type: dl.GeoJSON(
                 data=geojson_data,
-                id={'type': 'wotr-lines', 'ix': road_type},
+                id={'type': layer_type, 'ix': road_type},
                 zoomToBounds=True,
                 interactive=True,
-                hoverStyle={'color': TYPES_COLORS[road_type], 'weight': 6},
-                style={'color': TYPES_COLORS[road_type], 'weight': 3},
+                hoverStyle={'color': TYPES_COLORS[road_type], 'weight': 8},
+                style={'color': TYPES_COLORS[road_type], 'weight': 4},
                 bubblingMouseEvents=True,
                 children=[
                     dl.Tooltip(
@@ -193,6 +222,102 @@ def init_dash(dash_app):
             if (geojson_data := road_types_geojson_data.get(road_type))
         }
 
+    @dash_app.callback(
+        Output('wotr-search-suggestions', 'children'),
+        Output('wotr-submit-button', 'disabled'),
+        Input('wotr-road-name-input', 'value'),
+    )
+    def search_input_suggest(input_value: str):
+        """
+        Populate the search suggestions div.
+        """
+        road_name_clean = get_road_name_clean(input_value)
+        if len(road_name_clean) < 3:
+            return [], True
+
+        query = (
+            "SELECT DISTINCT \"Name\"\n"
+            "FROM \"Roads\".roads_clip\n"
+            "WHERE\n"
+            "  LOWER(REGEXP_REPLACE(\"Name\", '[^a-zA-Z0-9 ]', '', 'g')) LIKE '%%" + road_name_clean.replace(' ', '%%') + "%%'\n"
+        )
+        results = roads_pipe.instance_connector.exec(query).fetchall()
+        buttons = [
+            dbc.Button(
+                row[0],
+                color='link',
+                id={
+                    'type': 'wotr-search-suggest-button',
+                    'ix': json.dumps(
+                        {'Name': row[0]},
+                        separators=(',', ':'),
+                        sort_keys=True,
+                    ),
+                },
+                size='sm',
+                style={'text-decoration': 'none'},
+            )
+            for row in results
+        ]
+        return buttons, False
+
+    @dash_app.callback(
+        Output('wotr-results-table', 'children'),
+        Output('wotr-lines-group', 'children'),
+        Input('wotr-submit-button', 'n_clicks'),
+        Input('wotr-road-name-input', 'n_submit'),
+        State('wotr-road-name-input', 'value'),
+    )
+    def submit_click(n_clicks, n_submit, road_name):
+        if not road_name or len(road_name) < 3:
+            raise PreventUpdate
+
+        road_name_clean = get_road_name_clean(road_name)
+        query = (
+            "SELECT\n"
+            "  \"Name\",\n"
+            "  \"Type\",\n"
+            "  \"Owner\",\n"
+            "  \"Phone\",\n"
+            "  \"Email\",\n"
+            "  \"Online Form\",\n"
+            "  ST_Union(\"geometry\") AS \"geometry\"\n"
+            "FROM \"Roads\".roads\n"
+            "WHERE\n"
+            "  LOWER(REGEXP_REPLACE(\"Name\", '[^a-zA-Z0-9 ]', '', 'g')) LIKE '%%" + road_name_clean.replace(' ', '%%') + "%%'\n"
+            "GROUP BY\n"
+            "  \"Name\",\n"
+            "  \"Type\",\n"
+            "  \"Owner\",\n"
+            "  \"Contact\",\n"
+            "  \"Phone\",\n"
+            "  \"Email\",\n"
+            "  \"Online Form\"\n"
+            "ORDER BY \"Name\" ASC\n"
+            "LIMIT 100"
+        )
+        df = gpd.read_postgis(query, roads_pipe.instance_connector.engine, geom_col='geometry')
+        non_geo_cols = [col for col in df.columns if col != 'geometry']
+        road_types_geojson = get_road_types_geojson(df)
+        cards = [
+            dbc.Card(
+                dbc.CardBody(
+                    [
+                        dbc.Button(
+                            html.B(doc['Name']),
+                            color='dark',
+                            size='lg',
+                            id={
+                                'type': 'wotr-card-name-button',
+                                'ix': json.dumps(doc, separators=(',', ':'), sort_keys=True),
+                            },
+                        ),
+                    ] + build_tooltip_contents(doc, include_name=False)
+                )
+            )
+            for doc in df[non_geo_cols].to_dict(orient='records')
+        ]
+
         table_children = [
             html.Div(
                 html.P(
@@ -200,7 +325,7 @@ def init_dash(dash_app):
                     style={'color': '#999999', 'font-size': '12px', 'text-align': 'right'},
                 )
             ),
-            table,
+            build_cards_grid(cards, 2)
         ]
 
         return table_children, list(road_types_geojson.values())
@@ -241,7 +366,7 @@ def init_dash(dash_app):
             closeOnClick=False,
         )
 
-    def build_tooltip_contents(props):
+    def build_tooltip_contents(props, include_name: bool = True):
         content = []
         table_props = ['Type', 'Owner', 'Online Form', 'Email', 'Phone']
         table = html.Table(html.Tbody([
@@ -268,20 +393,23 @@ def init_dash(dash_app):
                 'borderSpacing': '20px 5px',
             },
         )
-        content.extend([
-            html.H6(html.B(props.get('Name', 'N/A'))),
-            table,
-        ])
+        if include_name:
+            content.append(html.H6(html.B(props.get('Name', 'N/A'))))
+        content.append(table)
         return content
 
 
-    def build_initial_map_layout(boundary_opacity: float = 0.4, fill_opacity: float = 0.1,):
+    def build_initial_map_layout(
+        boundary_opacity: float = 0.4,
+        fill_opacity: float = 0.1,
+        iframe_scroll: bool = False,
+    ):
         return dl.Map(
             children=[
                 dl.TileLayer(
                     url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-                ),
+                )] + ([dl.GestureHandling()] if iframe_scroll else []) + [
                 dl.GeoJSON(
                     url='https://meerschaum.io/files/bwg/output/geojson/Boundaries/boundaries_greenville.geojson',
                     id='wotr-boundaries-greenville',
@@ -295,7 +423,7 @@ def init_dash(dash_app):
                 dl.GeoJSON(
                     url='https://meerschaum.io/files/bwg/output/geojson/Boundaries/boundaries_simpsonville.geojson',
                     id='wotr-boundaries-simpsonville',
-                    style={'color': '#c6c6c6', 'fillColor': '#c6c6c6', 'weight': 2, 'opacity': boundary_opacity, 'fillOpacity': fill_opacity},
+                    style={'color': '#c6c6c6', 'fillColor': '#c6c6c6', 'weight': 2, 'opacity': 0.8, 'fillOpacity': 0.3},
                 ),
                 dl.GeoJSON(
                     url='https://meerschaum.io/files/bwg/output/geojson/Boundaries/boundaries_travelers_rest.geojson',
@@ -317,6 +445,7 @@ def init_dash(dash_app):
                     id='wotr-boundaries-county',
                     style={'color': '#333333', 'weight': 3, 'opacity': 0.5, 'fill': False,},
                 ),
+                dl.FeatureGroup(id='wotr-highlight-line-group', interactive=True),
                 dl.FeatureGroup(id='wotr-lines-group', interactive=True),
                 html.Div(id='wotr-click-marker-container'),
                 dl.FullScreenControl(),
